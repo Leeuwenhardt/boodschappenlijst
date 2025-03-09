8f363417d7297cc6c5cafb4e6979b4d22b7556f8
Leeuwenhardt <azurehigh5@gmail.com>
2025-03-09 23:44:24 +0100

    fixes

--- script/WerkenMetEenCodeEditor/dobbelstenen/script.js
+++ script/WerkenMetEenCodeEditor/dobbelstenen/script.js
@@ -1,12 +1,14 @@
-//document.getElementById('Button');
+const button = document.getElementById('button');
 const rng = [];
+const count = {}
 
 function rollDice() {
+    rng.length = 0;
     for (let i = 0; i < 8; i++) {
-    let num = Math.floor(Math.random() * 6 + 1)
+    let num = Math.floor(Math.random() * 6 + 1);
     rng.push(num);
     console.log(num)
-    };
+    }
 }
 
 button.addEventListener('click', rollDice);
