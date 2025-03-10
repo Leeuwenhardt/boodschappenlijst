a8100c82ba18f265e0b7fd1f43a57759ef73036c
Leeuwenhardt <azurehigh5@gmail.com>
2025-03-10 15:31:54 +0100

    1.0

--- script/WerkenMetEenCodeEditor/dobbelstenen/script.js
+++ script/WerkenMetEenCodeEditor/dobbelstenen/script.js
@@ -1,15 +1,37 @@
 const button = document.getElementById('button');
+const amount = document.getElementsByClassName('amount');
 const rng = [];
-const count = {}
+const count = {
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0,
+    6: 0    
+};
 
 function rollDice() {
+    //Manipulate rng array
     rng.length = 0;
     for (let i = 0; i < 8; i++) {
     let num = Math.floor(Math.random() * 6 + 1);
     rng.push(num);
-    console.log(num)
+    }
+    
+    //Manipulate count array
+    for (let num of rng) {
+        count[num]++
+        console.table(count)
+    }
+    //Manipulate table
+    for (let key in amount) {            
+        console.log(`{key}`);
+        })    
     }
 }
+
+
+
 
 button.addEventListener('click', rollDice);
 
