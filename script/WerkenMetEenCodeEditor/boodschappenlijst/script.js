bfb5bd6b8aa77e74db3be399d9c760f4bbf3fd27
Leeuwenhardt <azurehigh5@gmail.com>
2025-03-11 15:34:03 +0100

    1.0

--- script/WerkenMetEenCodeEditor/boodschappenlijst/script.js
+++ script/WerkenMetEenCodeEditor/boodschappenlijst/script.js
@@ -1,20 +1,26 @@
 const table = document.getElementById('table')
-const price = document.getElementsByClassName('productPrice');
-const quantity = document.getElementsByClassName('productQuantity');
-const total = document.getElementsByClassName('productTotalCost');
-const sum = document.getElementById('totalCost')
+const prices = document.getElementsByClassName('productPrice');
+const quantities = document.getElementsByClassName('productQuantity');
+const totals = document.getElementsByClassName('productTotalCost');
+const sum = document.getElementById('totalCost');
 
-table.addEventListener("change", (event) => {
-    console.log('Aantal producten is gewijzigd', event.target.value);
+table.addEventListener("input", (event) => {
+    if (event.target.classList.contains('productQuantity')) {
+        console.log('Aantal producten is gewijzigd:', event.target.value);
+        productChange(); 
+    }
 });
 
 function productChange() {
-    for (let i = 0; i < quantity.length; i++) {
-        console.log(i, price[i], quantity[i], total[i])
+    let totalSum = 0;
+    for (let i = 0; i < prices.length; i++) {
+        const price = prices[i].innerHTML;
+        const quantity = quantities[i].value;
+        totals = price * quantity;
+
+        totals[i].innerHTML = totals.toFixed(2);
+        totalSum += totals;
     } 
 };
 
-
-console.log(price)
-console.log(quantity)
-console.log(total)
+productChange();
